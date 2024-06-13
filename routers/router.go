0298f3656deb7854432233d03a73b4848de64121
Alberth Wu <alb3rth84@gmail.com>
2024-06-13 07:32:49 +0700

    Fixing
    Fixing

--- routers/router.go
+++ routers/router.go
@@ -62,14 +62,14 @@ func init() {
 	beego.Router("/v1/product/list/acc", &master.ProductController{}, "get:GetAllListAcc")
 	beego.Router("/v1/product/list/others", &master.ProductController{}, "get:GetAllListOthers")
 	beego.Router("/v1/product/list/recycle", &master.ProductController{}, "get:GetAllListRecycle")
-	beego.Router("/v1/product/list/uom", &master.ProductController{}, "get:GetProductUom")
+	beego.Router("/v1/product/uom/:id", &master.ProductController{}, "get:GetProductUom")
 	beego.Router("/v1/product/conversion/:id", &master.ProductController{}, "get:GetConversion")
 	// image
 	beego.Router("/v1/product/document/:id", &master.ProductController{}, "post:PostDocument;get:GetDocument")
 	// uom
-	beego.Router("/v1/product/uom", &master.UomController{}, "post:Post;get:GetAll")
-	beego.Router("/v1/product/uom/:id", &master.UomController{}, "put:Put;get:GetOne")
-	beego.Router("/v1/product/uom/list", &master.UomController{}, "get:GetAllList")
+	beego.Router("/v1/uom", &master.UomController{}, "post:Post;get:GetAll")
+	beego.Router("/v1/uom/:id", &master.UomController{}, "put:Put;get:GetOne")
+	beego.Router("/v1/uom/list", &master.UomController{}, "get:GetAllList")
 
 	// accounting
 	beego.Router("/v1/accounting/account_type/list", &accounting.AccountTypeController{}, "get:GetAllList")
